45b44ff8053ad9ec739b3e5c12c96e151461a767
Sandeep Bhatti <sandeepk9187@gmail.com>
2023-09-20 00:14:16 +0530

    Initial Commit

---
+++ networkmastry/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  networkmastry
+//
+//  Created by Pardip Bhatti on 20/09/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ networkmastry/networkmastryApp.swift
@@ -0,0 +1,17 @@
+//
+//  networkmastryApp.swift
+//  networkmastry
+//
+//  Created by Pardip Bhatti on 20/09/23.
+//
+
+import SwiftUI
+
+@main
+struct networkmastryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
